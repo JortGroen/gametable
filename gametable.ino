4f1eac76b25d3f5e6cb19d0f139a2885242380f4
Jort Groen <jort.groen@gmail.com>
2022-12-05 18:38:47 +0100

    added main menu functionallity

--- gametable.ino
+++ gametable.ino
@@ -2,15 +2,44 @@
 
 #include "gametable.h"
 #include "roulette.h"
+#include "pickatooth.h"
 
+uint8_t game;
 
 void setup() {
   tableinit();
 }
 
 void loop() {
-  roulette();
-  //test();
-  setAll(LOW);
+  
+  game = menu();
   delay(2000);
+  setAll(LOW);
+  
+  switch (game){
+    // green button
+    case 0: roulette();
+      break;
+      
+    // white button
+    case 1: roulette2();
+      break;
+      
+    // blue button
+    case 2: pickatooth();
+      break;
+
+    // red button
+    case 3: celebrate();
+      break;
+
+    // yellow button
+    case 4: 
+      uint8_t button = waitforpress();
+      if(button == BUTTON_RED){
+        digitalWrite(LED_RED, HIGH);
+        delay(1000);
+      }
+      break;
+  }
 }
